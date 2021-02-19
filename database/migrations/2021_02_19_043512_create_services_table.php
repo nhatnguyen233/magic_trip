b2586fee000220edf88600a336d7f4b1f8612d75
nhatnguyen233 <nhatguitarfs@gmail.com>
2021-02-19 11:48:58 +0700

    create migration version 1

---
+++ database/migrations/2021_02_19_043512_create_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('icon')->nullable();
+            $table->integer('status')->nullable();
+            $table->unsignedBigInteger('room_id')->nullable();
+            $table->timestamps();
+//            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
---
+++ database/migrations/2021_02_19_043931_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_id');
+            $table->integer('number_of_guests');
+            $table->integer('status')->nullable();
+            $table->timestamp('date_of_booking')->nullable();
+            $table->timestamps();
+//            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
