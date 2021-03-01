e5851af1fbd27a456b4fdb167c8288c4c4d04d4f
nhatnguyen233 <nhatguitarfs@gmail.com>
2021-03-01 20:22:03 +0700

    Fix migration v3

--- database/migrations/2021_02_26_091706_create_tour_attractions_table.php
+++ database/migrations/2021_02_26_091706_create_tour_attractions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelToursTable extends Migration
+class CreateTourAttractionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,10 +15,10 @@ class CreateTravelToursTable extends Migration
     {
         Schema::create('travel_tours', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('tourist_attraction_id')->nullable();
+            $table->unsignedBigInteger('attraction_id')->nullable();
             $table->unsignedBigInteger('tour_id')->nullable();
             $table->timestamps();
-//            $table->foreign('tourist_attraction_id')->references('id')->on('tourist_attractions');
+//            $table->foreign('attraction_id')->references('id')->on('tourist_attractions');
 //            $table->foreign('tour_id')->references('id')->on('tours');
         });
     }
