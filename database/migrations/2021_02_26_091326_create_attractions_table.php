b46e28549a0af11c5002de32a61b60fca6b83e1f
nhatnguyen233 <nhatguitarfs@gmail.com>
2021-03-03 20:39:19 +0700

    Update migration version 5

--- database/migrations/2021_02_26_091326_create_attractions_table.php
+++ database/migrations/2021_02_26_091326_create_attractions_table.php
@@ -17,11 +17,17 @@ class CreateAttractionsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
+            $table->unsignedBigInteger('country_id')->nullable();
             $table->unsignedBigInteger('province_id')->nullable();
+            $table->unsignedBigInteger('district_id')->nullable();
+            $table->unsignedBigInteger('ward_id')->nullable();
             $table->string('avatar')->nullable();
             $table->string('thumbnail')->nullable();
             $table->timestamps();
 //            $table->foreign('province_id')->references('id')->on('provinces');
+//            $table->foreign('country_id')->references('id')->on('countries');
+//            $table->foreign('district_id')->references('id')->on('districts');
+//            $table->foreign('ward_id')->references('id')->on('wards');
         });
     }
 
---
+++ database/migrations/2021_03_03_133504_create_attraction_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttractionImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attraction_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('attraction_id');
+            $table->string('url');
+            $table->timestamps();
+//            $table->foreign('attraction_id')->references('id')->on('attractions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attraction_images');
+    }
+}
