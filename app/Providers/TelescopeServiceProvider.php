bb2faec7b3f03ee53c8e2f8a9523eaec8157998a
Vanessa Nguyen <yenrion9941@gmail.com>
2021-04-16 15:02:33 +0700

    added postgres database configuration

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -5,9 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
-use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
-class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
+class TelescopeServiceProvider
 {
     /**
      * Register any application services.
